74db024365be6bc0411d2aaebc34d5e049c5bcbf
sunshine <sunshine.mahant@gmail.com>
2014-12-23 20:54:38 +0530

    composer

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,14 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('homestead'),
-
-));
+//$env = $app->detectEnvironment(array(
+//
+//	'local' => array('homestead'),
+//
+//));
+	$env = $app->detectEnvironment(function(){
+		return getenv("ENV");
+	});
 
 /*
 |--------------------------------------------------------------------------
